8499e24984a32857e52a8a6b8bb7087b166705f3
a.solis <solis.acevedo@gmail.com>
2019-12-25 14:09:47 +0100

    latest 191225

---
+++ GetandCleanData.R
@@ -0,0 +1,31 @@
+# Raw and process data ----
+# 1.- Raw data
+# 2.- Tidy data sets
+# 3.- code book - (metadata) describes each variable and value in the tidy data set
+# 4.- instruction list - steps mapping (how you got from A to B), add any other programming languages code also
+
+# Tidy data shouls have - one talbe per element, each variable in one column, 
+#each observation in one row, row 1 should have human readable names, 
+#foreign keys to loink tables, and one file per table
+
+# Downloading files ----
+getwd()
+setwd()
+
+# relative path
+setwd("./Getting_and_Cleaning_Data")
+# absolute path
+setwd("/Users/alejandrosolis/Desktop/Data_Sc/R/Getting_and_Cleaning_Data")
+
+
+
+
+
+
+
+
+
+
+
+
+
