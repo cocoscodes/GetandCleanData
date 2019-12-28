5efd3c1e7b45edb54e59024a90e1040d25db5613
a.solis <solis.acevedo@gmail.com>
2019-12-28 21:43:06 +0100

    latest 191228

--- GetandCleanData.R
+++ GetandCleanData.R
@@ -118,21 +118,41 @@ DT[2,]
 DT[DT$y =="a",]
 DT[c(2,3)] # without comma, only picks rows, it does not work with columns
 
+DT[,list(mean(x),sum(z))] # passing a list of functions for each column
+DT[,table(y)] # presenting a column in a table form
+
+DT[,w:=z^2] # adding a new column using the := operator
+DT
+
+DT[,m:={tmp <- (x+z);log2(tmp+5)}] # adding a new column by using multiple operations, separate the expressions by ;
+DT
+
+DT[,a:=x>0] # binary variables, plyr like operations
+DT
+
+DT[,b:=mean(x+w),by=a] # variable groupe by another column
+DT
+
+set.seed(123);
+DT<-data.table(x=sample(letters[1:3],1E5,TRUE))
+DT[,.N,by=x] # .N allows you to count the number of each letter appear, group by the x variable
+
+DT <- data.table(x=rep(c("a","b","c"),100),y=rnorm(300))
+setkey(DT,x) # setting keys for quick subsetting
+DT['a']
+
+DT1 <- data.table(x=c("a","a","b","dt1"),y=1:4)
+DT2 <- data.table(x=c("a","b","dt2"),z=5:7)
+setkey(DT1,x); setkey(DT2,x)
+merge(DT1,DT2)  
+
+big_df <- data.frame(x=rnorm(1E6),y=rnorm(1E6))  
+file <- tempfile()  
+write.table(big_df,file=file,row.names = FALSE,col.names = TRUE,sep = "\t",quote = FALSE)  
+system.time(fread(file))  # faster method to read a file
+system.time(read.table(file,header = TRUE,sep = "\t")) # way slower elapsed time
 
 
-
-
-
-
-
-
-
-  
-  
-  
-  
-  
-  
 
 
 
