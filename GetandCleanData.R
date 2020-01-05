75728b12723cea5d1d6d07c9bce1c567303be063
a.solis <solis.acevedo@gmail.com>
2020-01-05 22:40:40 +0100

    latest 200105

--- GetandCleanData.R
+++ GetandCleanData.R
@@ -444,7 +444,68 @@ fakeData = rnorm(1e5)
 object.size(fakeData)
 print(object.size(fakeData),units = "Mb")
 
+# Creating new variables ----
+# common variables to create: missing indicators, cutting up quatitative variables, and applying transform
+# creating sequences to index your data
+s1 <- seq(1,10,by=2); s1 # by adding two values
+s2 <- seq(1,10,length=3); s2 # lenght of the list
+x <- c(1,3,8,25,100); seq(along=x) # create an index the length of the number of values
+# logical operation that determines if they are near me
+restData$nearMe <- restData$neighborhood %in% c("Roland Park","Homeland")
+table(restData$nearMe)
+# create binary variables
+restData$zipWrong <- ifelse(restData$zipCode<0,TRUE,FALSE)
+table(restData$zipWrong,restData$zipCode<0)
+# creating categorical variables
+restData$zipGroups <- cut(restData$zipCode,breaks = quantile(restData$zipCode))
+table(restData$zipGroups)
+table(restData$zipGroups,restData$zipCode)
 
+install.packages("Hmisc")
+library(Hmisc)
+restData$zipGroups <- cut2(restData$zipCode,g=4) # easier cutting, g equal to quantiles
+table(restData$zipGroups)
+# creating factor variables
+restData$zcf <- factor(restData$zipCode)
+restData$zcf[1:10]
+class(restData$zcf)
+# levels of factor varialbes
+yesno <- sample(c("yes","no"),size = 10,replace = TRUE)
+yesnofac <- factor(yesno, levels = c("yes","no"))
+relevel(yesnofac,ref = "yes")
+as.numeric(yesnofac)
+
+library(plyr)
+restData2 = mutate(restData, zipGroups=cut2(zipCode,g=4)) # using mutate to create a new variable
+table(restData2$zipGroups)
+# common transforms
+x <- 3.475
+abs(x)
+sqrt(x)
+ceiling(x)
+floor(x)
+round(x,digits = 2)
+signif(x,digits = 2)
+cos(x)
+sin(x)
+log(x)
+log2(x)
+log10(x)
+exp(x)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
